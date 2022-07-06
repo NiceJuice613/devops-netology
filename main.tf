8b3fa605ca81046e70809029be5d6ab0b3777451
Ivan Ivanov <sin613@mail.ru>
2022-07-06 18:39:24 +0500

    Boru TF

--- main.tf
+++ main.tf
@@ -4,17 +4,31 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-}
+
+backend "s3" {
+  endpoint = "storage.yandexcloud.net"
+  bucket = "bucketiv"
+  region = "ru-central1"
+  key = "home/ivani/Terraform/"
+  access_key = "YCAJEs0KlHhn37Bp_Me7UCf0r"
+  secret_key = "YCNWf9WOFAqvw2oWOimHvHmTvDivHXYwm0GYx2-G"
+
+  skip_region_validation = true
+  skip_credentials_validation = true
+
+    }
+  }
 
 provider "yandex" {
-  token     = "XXXXXX"
+  token     = "AQAAAAA7ZUQNAATuwWJIT69ao0T3k2jNvzp3dwE"
   cloud_id  = "b1g346r19jbf2u7rrrfl"
   folder_id = "b1g52qq5gi6nmlq7sbs4"
+  #service_account_key_file = "ivanserv-key.json"
   zone      = "ru-central1-a"
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts-gpu"
+  family = "ubuntu-1804-lts-vgpu"
 }
 
 resource "yandex_vpc_network" "default" {
@@ -28,22 +42,23 @@ resource "yandex_vpc_subnet" "default" {
   zone           = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "ubuntIV" {
-  name        = "ubuntIV"
-  hostname    = "ubuntIV.netology.cloud"
+resource "yandex_compute_instance" "ivcent" {
+  name        = "ivcent"
+  zone        = "ru-central1-a"
+  hostname    = "ivcent.netology.cloud"
   allow_stopping_for_update = true
-
+  
   resources {
-    cores         = 2
-    memory        = 2
+    cores         = 8
+    memory        = 8
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd80d7fnvf399b1c207j"
-      name     = "root-ubuntIV"
-      type     = "network-hdd"
-      size     = "20"
+      image_id = "fd807ed79a4kkqfvd1mb"
+      name     = "root-ivcent"
+      type     = "network-nvme"
+      size     = "50"
     }
   }
 
